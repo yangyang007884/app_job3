f529fc1e39ec5ff7cfcd7c3a984a63199c691ec9
yangyang <pqbd5777@gmail.com>
2026-06-28 10:22:48 +0700

    fix allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -25,9 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2e75na_!_7h!^#c#yo7#@svgdyhzaqorvrmf$ugmko-c2e$bsq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['app-job3.onrender.com']
+DEBUG = False
 
 
 # Application definition
